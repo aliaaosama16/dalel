1ab5ee1d8d4fa83cd8803ed56dfb27cd1cce3d8a
akram yasser <akramkoyasser@gmail.com>
2022-03-03 15:11:25 +0200

    code page update

--- src/app/modules/auth/code/code.page.ts
+++ src/app/modules/auth/code/code.page.ts
@@ -6,24 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./code.page.scss'],
 })
 export class CodePage implements OnInit {
-  inputFocusPhone:boolean=false;
-  constructor() { }
+  inputFocusPhone: boolean = false;
+  codeValues: string;
+  code: number;
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  confirmVerificationCode() {
+    this.code = parseInt(this.codeValues);
+    console.log('code is :'+this.codeValues.substring(9));
   }
 
-
-  confirmVerificationCode(){
-
+  focusPhone(focusStatus: boolean) {
+    console.log('input focus' + focusStatus);
+    this.inputFocusPhone = focusStatus;
   }
 
-  focusPhone(focusStatus:boolean){
-    console.log('input focus'+focusStatus);
-    this.inputFocusPhone=focusStatus;
+  next(ev, nextInput) {
+    console.log(ev.target.value);
+
+    const input = ev.target;
+    const length = input.value.length;
+    console.log('length is ' + length);
+    const maxLength = input.attributes.maxlength.value;
+    console.log('maxLength is ' + maxLength);
+    this.codeValues += ev.target.value;
+    console.log(this.codeValues);
+    if (length >= maxLength) {
+      nextInput.setFocus();
+    }
+  
   }
 
-  next(ev,nextInput){
-   console.log(ev)
-   nextInput.setFocus()
+  focusFristNumber(){
+    
   }
 }
