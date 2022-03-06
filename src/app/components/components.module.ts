f75c4bc74c2e7becdad9ccc3743c9a1134d5f45f
akram yasser <akramkoyasser@gmail.com>
2022-03-06 16:21:14 +0200

    main page

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicModule } from '@ionic/angular';
+import { CustomButtonComponent } from './custom-button/custom-button.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CustomButtonComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
+    TranslateModule.forChild(),
+  ],
+  exports: [
+    CustomButtonComponent
   ]
 })
 export class ComponentsModule { }
