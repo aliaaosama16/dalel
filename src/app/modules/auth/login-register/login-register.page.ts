ca8cc824e8d20edb2817ce0186d03e2af44713ff
akram yasser <akramkoyasser@gmail.com>
2022-03-06 11:23:01 +0200

    auth validation

--- src/app/modules/auth/login-register/login-register.page.ts
+++ src/app/modules/auth/login-register/login-register.page.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LanguageService } from 'src/app/services/language/language.service';
-
+import {
+  FormGroup,
+  Validators,
+  FormBuilder,
+  FormControl,
+  AbstractControl,
+  ValidatorFn,
+} from '@angular/forms';
 @Component({
   selector: 'app-login-register',
   templateUrl: './login-register.page.html',
@@ -9,80 +16,137 @@ import { LanguageService } from 'src/app/services/language/language.service';
 })
 export class LoginRegisterPage implements OnInit {
   currentlangauge: string;
-  authType:string='register';
-  inputFocusPerson:boolean=false;
-  inputFocusPersonIcon:string='./../../../../assets/icon/person-active.svg';
-  inputInFocusPersonIcon:string='./../../../../assets/icon/person-inactive.svg';
+  isRegisterSubmitted = false;
+  authType: string = 'register';
+  inputFocusPerson: boolean = false;
+  inputFocusPersonIcon: string = './../../../../assets/icon/person-active.svg';
+  inputInFocusPersonIcon: string =
+    './../../../../assets/icon/person-inactive.svg';
 
-  inputFocusEmail:boolean=false;
-  inputFocusEmailIcon:string='./../../../../assets/icon/email-active.svg';
-  inputInFocusEmailIcon:string='./../../../../assets/icon/email-inactive.svg';
+  inputFocusEmail: boolean = false;
+  inputFocusEmailIcon: string = './../../../../assets/icon/email-active.svg';
+  inputInFocusEmailIcon: string =
+    './../../../../assets/icon/email-inactive.svg';
 
-  inputFocusPhone:boolean=false;
-  inputFocusPhoneIcon:string='./../../../../assets/icon/phone-active.svg';
-  inputInFocusPhoneIcon:string='./../../../../assets/icon/phone-inactive.svg';
+  inputFocusPhone: boolean = false;
+  inputFocusPhoneIcon: string = './../../../../assets/icon/phone-active.svg';
+  inputInFocusPhoneIcon: string =
+    './../../../../assets/icon/phone-inactive.svg';
 
-  inputFocusPassword:boolean=false;
-  inputFocusPasswordIcon:string='./../../../../assets/icon/password-active.svg';
-  inputInFocusPasswordIcon:string='./../../../../assets/icon/password-inactive.svg';
+  inputFocusPassword: boolean = false;
+  inputFocusPasswordIcon: string =
+    './../../../../assets/icon/password-active.svg';
+  inputInFocusPasswordIcon: string =
+    './../../../../assets/icon/password-inactive.svg';
 
-  inputFocusConfirmPassword:boolean=false;
-  inputFocusConfirmPasswordIcon:string='./../../../../assets/icon/password-active.svg';
-  inputInFocusConfirmPasswordIcon:string='./../../../../assets/icon/password-inactive.svg';
-  
+  inputFocusConfirmPassword: boolean = false;
+  inputFocusConfirmPasswordIcon: string =
+    './../../../../assets/icon/password-active.svg';
+  inputInFocusConfirmPasswordIcon: string =
+    './../../../../assets/icon/password-inactive.svg';
+
+  public registerForm: FormGroup;
+  public signinForm: FormGroup;
   constructor(
     private langaugeservice: LanguageService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
-    this.currentlangauge=this.langaugeservice.getLanguage();
+    this.currentlangauge = this.langaugeservice.getLanguage();
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.registerForm = this.formBuilder.group({
+      userName: ['', [Validators.required, Validators.minLength(2)]],
+      phoneNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^05/),
+          Validators.minLength(10),
+          Validators.maxLength(10),
+          //10
+        ],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+    this.signinForm = this.formBuilder.group({
+      userName: ['', [Validators.required, Validators.minLength(2)]],
+      phoneNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^05/),
+          Validators.minLength(10),
+          Validators.maxLength(10),
+          //10
+        ],
+      ],
+    });
   }
 
   authTypeChange(ev: any) {
     console.log('Segment changed', ev.detail.value);
   }
 
-  signUp(){
-     // api call for register new account  
-     
-     this.router.navigateByUrl('/phone-number');
+  get errorControl() {
+    return this.registerForm.controls;
   }
 
-  signIn(){
-     // api call for login current user    
+  signUp() {
+    // api call for register new account
+    this.isRegisterSubmitted = true;
+    if (!this.registerForm.valid) {
+      console.log('Please provide all the required values!');
+      return false;
+    } else {
+      console.log(this.registerForm.value);
+    }
+    console.log(this.registerForm.value);
+    this.router.navigateByUrl('/phone-number');
   }
 
-  forgetPassword(){
+  signIn() {
+    // api call for login current user
+  }
+
+  forgetPassword() {
     this.router.navigateByUrl('/password');
   }
 
-  changAuthType(authType:string){
-    this.authType=authType;
+  changAuthType(authType: string) {
+    this.authType = authType;
   }
 
-  focusPerson(focusStatus:boolean){
-    console.log('input focus'+focusStatus);
-    this.inputFocusPerson=focusStatus;
+  // change icon beside input : just for style
+  focusPerson(focusStatus: boolean) {
+    console.log('input focus' + focusStatus);
+    this.inputFocusPerson = focusStatus;
   }
 
-  focusEmail(focusStatus:boolean){
-    console.log('input focus'+focusStatus);
-    this.inputFocusEmail=focusStatus;
+  focusEmail(focusStatus: boolean) {
+    console.log('input focus' + focusStatus);
+    this.inputFocusEmail = focusStatus;
   }
 
-  focusPhone(focusStatus:boolean){
-    console.log('input focus'+focusStatus);
-    this.inputFocusPhone=focusStatus;
+  focusPhone(focusStatus: boolean) {
+    console.log('input focus' + focusStatus);
+    this.inputFocusPhone = focusStatus;
   }
 
-  focusPassword(focusStatus:boolean){
-    console.log('input focus'+focusStatus);
-    this.inputFocusPassword=focusStatus;
+  focusPassword(focusStatus: boolean) {
+    console.log('input focus' + focusStatus);
+    this.inputFocusPassword = focusStatus;
   }
 
-  focusConfirmPassword(focusStatus:boolean){
-    console.log('input focus'+focusStatus);
-    this.inputFocusConfirmPassword=focusStatus;
+  focusConfirmPassword(focusStatus: boolean) {
+    console.log('input focus' + focusStatus);
+    this.inputFocusConfirmPassword = focusStatus;
   }
 }
