87b0766bdef7e583f6622e556573df009dc3bc41
akram yasser <akramkoyasser@gmail.com>
2022-03-14 15:38:05 +0200

    details

--- src/app/modules/category-details/category-details.page.ts
+++ src/app/modules/category-details/category-details.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { LanguageService } from 'src/app/services/language/language.service';
 
 @Component({
   selector: 'app-category-details',
@@ -7,14 +8,31 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./category-details.page.scss'],
 })
 export class CategoryDetailsPage implements OnInit {
+  currentlangauge: string;
 
-  constructor(private menuCtrl:MenuController) { }
+  itemDetails:any={
+    id: 1,
+    categoryID: 1,
+    name: 'اسم تجريبي',
+    city: 'الرياض',
+    address:'حي الرمال',
+    rating: '3+',
+    price: 3000,
+    unit: 'ر.س',
+    perUnit:'ليلة',
+    isFav:true
+  }
+  constructor(
+    private menuCtrl: MenuController,
+    private langaugeservice: LanguageService
+  ) {}
 
   ngOnInit() {
+    this.currentlangauge = this.langaugeservice.getLanguage();
+    console.log(this.currentlangauge);
   }
 
-  openMenu(){
+  openMenu() {
     this.menuCtrl.open();
   }
-
 }
