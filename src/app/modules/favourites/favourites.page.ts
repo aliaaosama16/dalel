65a4f33c8198fd566868b44f59589d4969bfcfad
akram yasser <akramkoyasser@gmail.com>
2022-03-14 13:06:59 +0200

    refactor : remove comments

--- src/app/modules/favourites/favourites.page.ts
+++ src/app/modules/favourites/favourites.page.ts
@@ -6,50 +6,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favourites.page.scss'],
 })
 export class FavouritesPage implements OnInit {
-
-  favourites:any[]=[
+  favourites: any[] = [
     {
-      name:'اسم تجريبي',
-      city:'الرياض',
-      rating:'3+',
-      price:3000,
-      unit:'ر.س'
+      id: 1,
+      categoryID: 1,
+      name: 'اسم تجريبي',
+      city: 'الرياض',
+      rating: '3+',
+      price: 3000,
+      unit: 'ر.س',
     },
     {
-      name:'اسم تجريبي',
-      city:'الرياض',
-      rating:'3+',
-      price:3000,
-      unit:'ر.س'
-    },{
-      name:'اسم تجريبي',
-      city:'الرياض',
-      rating:'3+',
-      price:3000,
-      unit:'ر.س'
-    },{
-      name:'اسم تجريبي',
-      city:'الرياض',
-      rating:'3+',
-      price:3000,
-      unit:'ر.س'
-    },{
-      name:'اسم تجريبي',
-      city:'الرياض',
-      rating:'3+',
-      price:3000,
-      unit:'ر.س'
-    },{
-      name:'اسم تجريبي',
-      city:'الرياض',
-      rating:'3+',
-      price:3000,
-      unit:'ر.س'
+      id: 2,
+      categoryID: 1,
+      name: 'اسم تجريبي',
+      city: 'الرياض',
+      rating: '3+',
+      price: 3000,
+      unit: 'ر.س',
     },
-  ]
-  constructor() { }
+    {
+      id: 3,
+      categoryID: 1,
+      name: 'اسم تجريبي',
+      city: 'الرياض',
+      rating: '3+',
+      price: 3000,
+      unit: 'ر.س',
+    },
+    {
+      id: 4,
+      categoryID: 1,
+      name: 'اسم تجريبي',
+      city: 'الرياض',
+      rating: '3+',
+      price: 3000,
+      unit: 'ر.س',
+    },
+    {
+      id: 5,
+      categoryID: 1,
+      name: 'اسم تجريبي',
+      city: 'الرياض',
+      rating: '3+',
+      price: 3000,
+      unit: 'ر.س',
+    },
+    {
+      id: 6,
+      categoryID: 1,
+      name: 'اسم تجريبي',
+      city: 'الرياض',
+      rating: '3+',
+      price: 3000,
+      unit: 'ر.س',
+    },
+  ];
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
